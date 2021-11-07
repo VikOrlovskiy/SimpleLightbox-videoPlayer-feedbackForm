2f5bfe5460b5481404905379aeb50d23f60093eb
vik_orel <vikorel34@gmail.com>
2021-11-07 10:28:02 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galeryComponentRef = document.querySelector('.gallery');
+const addedGalleryCard = galleryCardsCreator(galleryItems);
+// ------------------functionCreator---------------------------
+function galleryCardsCreator(gallery) {
+  return gallery
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+     
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+galeryComponentRef.insertAdjacentHTML('beforeend', addedGalleryCard);
 
-console.log(galleryItems);
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {});
