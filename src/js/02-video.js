5a3e3fe2ade1f06ba7f94687d9e14a04b054bd5b
vik_orel <vikorel34@gmail.com>
2021-11-07 10:28:14 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
-
+// ------------------Const---------------------------
+const iFrameRef = document.querySelector('#vimeo-player');
+const throttle = require('lodash.throttle');
+// ------------------ConnectVimeoPlayer--npm-------------------------
+const player = new Vimeo.Player(iFrameRef);
+// ------------------functionSave---------------------------
+function saveCurrentPlayerTime(date) {
+  localStorage.setItem('videoplayer-current-time', Math.round(date.seconds));
+  console.log(Number(localStorage.getItem('videoplayer-current-time')));
+}
+player.on('timeupdate', throttle(saveCurrentPlayerTime, 1000));
+// ------------------functionsetup---------------------------
+function setupCurrentPlayerTime() {
+  player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+}
+window.onload = setupCurrentPlayerTime;
