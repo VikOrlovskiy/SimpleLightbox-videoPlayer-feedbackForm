dc42d4c573ef324f77f5c9103b10fe9653f882dd
vik_orel <vikorel34@gmail.com>
2021-11-07 19:10:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
-
+// ------------------Const---------------------------
+const throttle = require('lodash.throttle');
+const formREf = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const feedbackFormState = {};
+// ------------------------------------------------
+function saveInputData(e) {
+  localStorage.setItem(STORAGE_KEY, feedbackFormState);
+  feedbackFormState[e.target.name] = e.target.value;
+}
+formREf.addEventListener('input', throttle(saveInputData, 500));
+// ------------------------------------------------
+formREf.addEventListener('submit', function (e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(feedbackFormState);
+});
