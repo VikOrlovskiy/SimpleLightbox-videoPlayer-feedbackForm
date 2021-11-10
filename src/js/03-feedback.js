62395af0f8f3092dca987655a36450b24de3efe2
vik_orel <vikorel34@gmail.com>
2021-11-10 09:13:50 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,32 @@
 // ------------------Const---------------------------
 const throttle = require('lodash.throttle');
 const formREf = document.querySelector('.feedback-form');
+const emailREf = formREf.querySelector('input');
+const textREf = formREf.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
-const feedbackFormState = {};
+let feedbackFormState = {};
+// ------------------------------------------------
+function returnSevedMessage() {
+  let parstLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parstLocalStorage) {
+    emailREf.value = parstLocalStorage.email;
+    textREf.value = parstLocalStorage.message;
+  }
+
+  // console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+}
+// returnSevedMessage();
+window.addEventListener('load', returnSevedMessage);
 // ------------------------------------------------
 function saveInputData(e) {
-  localStorage.setItem(STORAGE_KEY, feedbackFormState);
   feedbackFormState[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
 }
 formREf.addEventListener('input', throttle(saveInputData, 500));
 // ------------------------------------------------
 formREf.addEventListener('submit', function (e) {
   e.preventDefault();
   e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
   localStorage.removeItem(STORAGE_KEY);
-  console.log(feedbackFormState);
 });
